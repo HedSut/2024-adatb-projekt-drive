ffc32b4284d4c072e556b3b49ca453179cf0132e
tudhatnad <zoltan20040605@gmail.com>
2024-03-24 11:30:43 +0100

    adatok létrehozása

--- record-generator.py
+++ record-generator.py
@@ -1,4 +1,4 @@
-#used to generate a text file containing sql insertions with random values
+ #used to generate a text file containing sql insertions with random values
 #syntax <column_name>:<type> <column_name>:<type> ...
 #possible types: [i, ia] - integer, vc<number> - varchar, d - date
 
@@ -22,23 +22,23 @@ with open("inserts.txt", "w") as f:
 
 
         #create first part of insert statement
-        statement = f"INSERT INTO {table_name} ("
+        statement = f"INSERT INTO \"{table_name}\" ("
         for x in column_data:
-            statement += f"{x[0]}, "
+            statement += f"\"{x[0]}\", "
         statement = statement[:-2]
         statement += ") VALUES ("
         
         #generate values according to syntax map
         for x in column_data:
             if (x[1] == "i"):
-                statement += f"{random.randint(0, n*n)}, "
+                statement += f"\'{random.randint(0, n*n)}\', "
             elif (x[1] == "ia"):
-                statement += f"{i}, "
+                statement += f"\'{i}\', "
             elif (x[1][0:2] == "vc"):
-                statement += f"{"".join(choice(chars) for i in range(int(x[1][2:])))}, "
+                statement += f"\'{"".join(choice(chars) for i in range(int(x[1][2:])))}\', "
             elif (x[1] == "d"):
-                #INSERT INTO table VALUES (DATETIME 'YYYY-MM-DD HH:MI:SS');
-                statement += f"DATETIME '{random.randint(2000, 2024)}-{random.randint(1, 12)}-{random.randint(1, 28)} {random.randint(0, 23)}:{random.randint(0, 59)}:{random.randint(0, 59)}', "
+                #INSERT INTO table VALUES (TIMESTAMP 'YYYY-MM-DD HH:MI:SS');
+                statement += f"TIMESTAMP '{random.randint(2000, 2024)}-{random.randint(1, 12)}-{random.randint(1, 28)} {random.randint(0, 23)}:{random.randint(0, 59)}:{random.randint(0, 59)}', "
 
         #trim character from end and terminate statement        
         statement = statement[:-2]
