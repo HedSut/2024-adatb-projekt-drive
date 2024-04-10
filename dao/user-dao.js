345d88cfbb1f242d02d2a5949b2ca53498c06ff4
HedSut <bence02.info@gmail.com>
2024-04-10 10:47:40 +0200

    userdao edit

--- dao/user-dao.js
+++ dao/user-dao.js
@@ -1,14 +1,39 @@
 const oracledb = require("oracledb");
 
 class UserDao {
+    async addUser(username, email, password) {
+        let con = await oracledb.getConnection();
+        let result = await con.execute('INSERT INTO "users" VALUES (:user, :email, :pass)', {user: username, email: email, pass: password});
+        con.close();
+        return result.rows;
+    }
+
     async getAllUsers() {
         let con = await oracledb.getConnection();
-        let result = await con.execute('SELECT * FROM "bookmark"');
-
-        console.log(result);
-        //return results.resultSet;
+        let result = await con.execute('SELECT * FROM "users"');
         con.close();
-        return;
+        return result.rows;
+    }
+
+    async getUsers(username) {
+        let con = await oracledb.getConnection();
+        let result = await con.execute('SELECT * FROM "users" WHERE username = :user', {user: username});
+        con.close();
+        return result.rows;
+    }
+
+    async changeEmail(username, email) {
+        let con = await oracledb.getConnection();
+        let result = await con.execute('UPDATE "users" SET email = :email WHERE username = :user', {user: username, email: email});
+        con.close();
+        return result.rows;
+    }
+
+    async deleteUser() {
+        let con = await oracledb.getConnection();
+        let result = await con.execute('DELETE FROM "users" WHERE username = :user', {user: username});
+        con.close();
+        return result.rows;
     }
 }
 
