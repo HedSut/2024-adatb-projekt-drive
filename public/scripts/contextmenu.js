21dde70f5d592bc59014103c2ba71164559a34e8
tudhatnad <zoltan20040605@gmail.com>
2024-04-16 18:23:39 +0200

    megvan a letöltés frontend

--- public/scripts/contextmenu.js
+++ public/scripts/contextmenu.js
@@ -1,35 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
     const explorer = document.getElementById("explorer");
     const menuExplorer = document.getElementById("contextMenuExplorer");
-
+    const downloadButton = document.getElementById("downloadButton");
+    
+    const file = document.querySelectorAll('.file');
     const folder = document.querySelectorAll('.folder');
     const menuFolder = document.getElementById("contextMenuFolder");
 
+    function hideMenu() {
+        if (!menuFolder.classList.contains("hidden")) {
+            menuFolder.classList.add("hidden");
+        }
+
+        if (!menuExplorer.classList.contains("hidden")) {
+            menuExplorer.classList.add("hidden");
+        }
+
+        if(!downloadButton.classList.contains("hidden")) {
+            downloadButton.classList.add("hidden")
+        }
+    }
+
     explorer.addEventListener("contextmenu", function (event) {
-        if (!event.target.closest(".folder")) {
+        if (!event.target.closest(".folder") && !event.target.closest(".file")) {
+            hideMenu();
             event.preventDefault(); // Prevent the default context menu
             const x = event.clientX;
             const y = event.clientY;
             menuExplorer.style.left = x + "px";
             menuExplorer.style.top = y + "px";
             menuExplorer.classList.remove("hidden");
-
-            if (!menuFolder.contains(event.target)) {
-                menuFolder.classList.add("hidden");
-            }
         }
 
     });
 
     for (let i = 0; i < folder.length; i++) {
         folder[i].addEventListener("contextmenu", function (event) {
-
+            hideMenu();
             let target = event.target
             if (!(event.target instanceof HTMLDivElement)) {
                 target = target.parentNode;
             }
             document.getElementById("menuRenameId").innerHTML = target.id;
-
 
             event.preventDefault(); // Prevent the default context menu
             const x = event.clientX;
@@ -37,21 +49,30 @@ document.addEventListener("DOMContentLoaded", function () {
             menuFolder.style.left = x + "px";
             menuFolder.style.top = y + "px";
             menuFolder.classList.remove("hidden");
+        });
+    }
 
-            if (!menuExplorer.contains(event.target)) {
-                menuExplorer.classList.add("hidden");
+    for (let i = 0; i < file.length; i++) {
+        file[i].addEventListener("contextmenu", function (event) {
+            hideMenu();
+            let target = event.target
+            if (!(event.target instanceof HTMLDivElement)) {
+                target = target.parentNode;
             }
+
+            event.preventDefault(); // Prevent the default context menu
+            const x = event.clientX;
+            const y = event.clientY;
+            menuFolder.style.left = x + "px";
+            menuFolder.style.top = y + "px";
+            menuFolder.classList.remove("hidden");
+            downloadButton.classList.remove("hidden");
+            downloadButton.href="/download/" + target.id;
         });
     }
 
     document.addEventListener("click", function (event) {
-        if (!menuExplorer.contains(event.target)) {
-            menuExplorer.classList.add("hidden");
-        }
-
-        if (!menuFolder.contains(event.target)) {
-            menuFolder.classList.add("hidden");
-        }
+        hideMenu();
 
         if (event.target !== document.getElementById("renameButton")) {
             let id = document.getElementById("menuRenameId").innerHTML;
