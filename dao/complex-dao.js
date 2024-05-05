da434f3de697407a6a90f13e47c6ec73caf406b4
HedSut <bence02.info@gmail.com>
2024-05-05 22:15:55 +0200

    2 nagyon komplex lekérdezés

--- dao/complex-dao.js
+++ dao/complex-dao.js
@@ -44,6 +44,32 @@ class ComplexDao {
         console.log("Selected file bookmarks statistics\n");
         return result.rows;
     }
+
+    async getFolderStats() {
+        let con = await oracledb.getConnection();
+        let result = await con.execute(
+            'Select "folder"."folder_name", "folder"."owner_user" , cpe2.cnt from "folder" \
+            INNER JOIN (select "folder"."id", COUNT("file"."id") AS cnt FROM "folder" INNER JOIN "file" ON \
+            "folder"."id" = "file"."parent_id" GROUP BY  "folder"."id") cpe2 ON "folder"."id" = cpe2."id"',
+            { }
+        );
+        con.close();
+        console.log("Selected folder statistics\n");
+        return result.rows;
+    }
+
+    async getChildfolderStats() {
+        let con = await oracledb.getConnection();
+        let result = await con.execute(
+            'SELECT f1."folder_name", f1."owner_user", f2.child_folder_count FROM "folder" f1 \
+            INNER JOIN (SELECT "parent_id", COUNT("id") AS child_folder_count FROM "folder" GROUP BY "parent_id") f2 \
+            ON f1."id" = f2."parent_id"',
+            { }
+        );
+        con.close();
+        console.log("Selected folder statistics\n");
+        return result.rows;
+    }
 }
 
 module.exports = ComplexDao;
